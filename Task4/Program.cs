291e2ef305b4862aabb3ec22b7f9c231d1ef15d5
Сергей Комаров <komarov@smartdriving.io>
2024-01-13 19:55:32 +0300

    add Task4

---
+++ Task4/Program.cs
@@ -0,0 +1,13 @@
+﻿// int n = 5;
+int[] array = { 5, 6, 8, 20, 12 };
+int i=0;
+int max = array[0];
+while(i < array.Length)
+{
+    if (array[i] > max)
+    {
+        max = array[i];
+    }
+    i++;
+}
+Console.WriteLine(max);
