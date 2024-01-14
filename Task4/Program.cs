e18c78a22637d85c535e8864fb2115add8946e77
Skomarov <Serega@SegaPC>
2024-01-14 14:22:38 +0300

    add proramm Task4

--- Task4/Program.cs
+++ Task4/Program.cs
@@ -1,13 +1,34 @@
 ﻿// int n = 5;
-int[] array = { 5, 6, 8, 20, 12 };
-int i=0;
+// int[] array = { 5, 6, 8, 20, 12 };
+// int i=0;
+// int max = array[0];
+// while(i < array.Length)
+// {
+//     if (array[i] > max)
+//     {
+//         max = array[i];
+//     }
+//     i++;
+// }
+// Console.WriteLine(max);
+
+int[] array = { 5, 56, 8, 20, 40 };
 int max = array[0];
-while(i < array.Length)
+for (int i = 0;  i < array.Length; i++)
 {
-    if (array[i] > max)
+    if (max < array[i])
     {
-        max = array[i];
+    max = array[i];
     }
-    i++;
-}
+}    
 Console.WriteLine(max);
+
+ max = array[0];
+ foreach(int e in array)
+ {
+    if(e > max)
+    {
+        max = e;
+    }
+ }
+ Console.WriteLine(max);
--- Task4/obj/Debug/net8.0/Task4.AssemblyInfo.cs
+++ Task4/obj/Debug/net8.0/Task4.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Task4")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ae5f49b9675681edb6f5bc85b7e0c1994ed0c1e5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Task4")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Task4")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
